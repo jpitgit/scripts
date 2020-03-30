11161d3e63f7aa2a7d3bbec2e19a212ced67d1e4
jpitgit <51877105+jpitgit@users.noreply.github.com>
2020-03-30 23:36:05 +0200

    Create DefaultApps-JP.ps1

---
+++ DefaultApps-JP.ps1
@@ -0,0 +1,33 @@
+# Call this script from a powershell command prompt using this command:
+# Invoke-WebRequest -uri ""
+
+# Preparation
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Set-ExecutionPolicy RemoteSigned -Force
+New-Item -Path C:\ -Name Scripts -ItemType Directory
+
+# Install RSAT
+Install-WindowsFeature -IncludeAllSubFeature RSAT
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -uri "https://chocolatey.org/install.ps1" -UseBasicParsing | Invoke-Expression
+# Chocolatey tools
+Choco install chocolatey-gui -y
+
+# Essential tools
+Choco install notepadplusplus -y
+Choco install googlechrome -y
+Choco install 7zip -y
+Choco install winscp -y
+Choco install openssh -y
+Choco install sharex -y
+choco install microsoft-edge -y
+choco install putty -y
+choco install mremoteng -y
+choco install teamviewer -y
+
+# Microsoft Tools
+Choco install vscode -y
+Choco install vscode-powershell -y
+
+# Reboot to complete installation
+Restart-Computer
